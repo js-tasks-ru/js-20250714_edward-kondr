276f31a9e46a2e5a666f7dd10a1b5b579c4384c2
v1ssar1on <122610076+v1ssar1on@users.noreply.github.com>
2025-07-28 07:07:49 +0300

    feat: done 03 objects

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,29 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let result = "";
+  // счетчик одинаковых
+  let count = 0;
+  // последний символ
+  let lastChar = "";
 
+  for (const char of string) {
+    // если строго, то делаем счетчику плюс
+    if (char === lastChar) {
+      count++;
+    } else {
+      // в ином случае будет равен 1
+      count = 1;
+    }
+
+    // если счетчик меньше или равен размеру, то результат плюс char, '' + 'xxx'
+    if (count <= size) {
+      result += char;
+    }
+
+    // последний символ равен символу
+    lastChar = char;
+  }
+
+  return result;
 }
